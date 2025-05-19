4e4d3a1d97bb943217547c72aa8dc1107d0d83e0
Elijah Gillit <egillit@gmail.com>
2025-05-18 19:04:39 -0600

    init commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const allNotesNatural = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+
+for (let i = 0; i < allNotesNatural.length; i++) {
+	console.log(allNotesNatural[i]);
+}
+
+class keys {
+	getKeys() {}
+}
