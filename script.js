4dba53e659066000a08ad23b9153887851fc7de7
Elijah Gillit <egillit@gmail.com>
2025-05-19 17:10:30 -0600

    map for all base notes in a given key

--- script.js
+++ script.js
@@ -3,27 +3,35 @@ const NUMNOTES = 8;
 const allNotesNatural = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
 
 function getNotesInKey(key) {
-	let notes = {};
+	let objectNotes = new Map();
 	for (note in allNotesNatural) {
+		let keyindex = 1;
+
 		if (allNotesNatural[note] == key) {
 			for (let i = note; i <= NUMNOTES; i++) {
 				if (allNotesNatural[i] != undefined) {
+					objectNotes.set(keyindex, allNotesNatural[i]);
+
+					keyindex++;
 					console.log(allNotesNatural[i]);
 				}
 				if (i > allNotesNatural.length) {
-					for (
-						let j = allNotesNatural.length - i + 1;
-						j <= NUMNOTES - i + 1;
-						j++
-					) {
-						console.log('j ', allNotesNatural[j]);
+					let elemsAtStart =
+						(NUMNOTES - allNotesNatural.length - note - 1) * -1;
+
+					for (let j = 0; j < elemsAtStart; j++) {
+						objectNotes.set(keyindex, allNotesNatural[j]);
+
+						keyindex++;
 					}
 				}
 			}
 		}
 	}
+	return objectNotes;
 }
-getNotesInKey('c');
+const myNotes = getNotesInKey('c');
+console.log(myNotes);
 
 class keys {
 	getKeys() {}
