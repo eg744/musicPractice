e9863a49e0a95742dbd2a72ab53f1f0878fa396e
Elijah Gillit <egillit@gmail.com>
2025-05-18 22:08:57 -0600

    basic scale printing

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
+const NUMNOTES = 8;
+
 const allNotesNatural = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
 
-for (let i = 0; i < allNotesNatural.length; i++) {
-	console.log(allNotesNatural[i]);
+function getNotesInKey(key) {
+	let notes = {};
+	for (note in allNotesNatural) {
+		if (allNotesNatural[note] == key) {
+			for (let i = note; i <= NUMNOTES; i++) {
+				if (allNotesNatural[i] != undefined) {
+					console.log(allNotesNatural[i]);
+				}
+				if (i > allNotesNatural.length) {
+					for (
+						let j = allNotesNatural.length - i + 1;
+						j <= NUMNOTES - i + 1;
+						j++
+					) {
+						console.log('j ', allNotesNatural[j]);
+					}
+				}
+			}
+		}
+	}
 }
+getNotesInKey('c');
 
 class keys {
 	getKeys() {}
