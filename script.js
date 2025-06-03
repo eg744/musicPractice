8ccdb26680c1211e3abc8a849c0f848272d9e0e2
Elijah Gillit <egillit@gmail.com>
2025-06-03 15:24:01 -0600

    different approach using array, base chromatic scale, pattern for majors

--- script.js
+++ script.js
@@ -1,20 +1,55 @@
 import { LinkedList } from '/modules/Linkedlist.js';
 
-let list = new LinkedList();
-list.add(1);
-list.add(2);
-list.add(4);
-list.add(5);
+// let list = new LinkedList();
+// list.add(1);
+// list.add(2);
+// list.add(4);
+// list.add(5);
 
-list.insertBetween(3, 2); // Insert 3 after 2
-list.print(); // Output: 1 -> 2 -> 3 -> 4 -> 5
+// list.insertBetween(3, 2); // Insert 3 after 2
+// list.print(); // Output: 1 -> 2 -> 3 -> 4 -> 5
 
 const NUMNOTES = 8;
 
-const allNotesNatural = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+const chromaticScale = [
+	'A',
+	'A#',
+	'B',
+	'C',
+	'C#',
+	'D',
+	'D#',
+	'E',
+	'F',
+	'F#',
+	'G',
+	'G#',
+];
+const allNotesNatural = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+const scalesWithSharps = ['G', 'D', 'A', 'E'];
+const scalesWithFlats = ['F', 'Bb', 'Eb', 'Eb'];
+const majorScalePattern = [2, 2, 1, 2, 2, 2, 1];
+
+function getMajorScale(root) {
+	const startIndex = chromaticScale.indexOf(root);
+	if (startIndex === -1) {
+		throw new Error('Invalid root note');
+	}
+
+	let scale = [root];
+	let currentIndex = startIndex;
+
+	for (let step of majorScalePattern) {
+		currentIndex = (currentIndex + step) % chromaticScale.length;
+		scale.push(chromaticScale[currentIndex]);
+	}
+
+	return scale;
+}
+
+console.log('gms', getMajorScale('C#'));
 
 function getNotesInKey(key) {
-	let objectNotes = new Map();
 	let arrayNotes = [];
 	for (let note in allNotesNatural) {
 		let keyindex = 1;
@@ -43,7 +78,7 @@ function getNotesInKey(key) {
 	return arrayNotes;
 	// return objectNotes;
 }
-const myNotes = getNotesInKey('c');
+// const myNotes = getNotesInKey('C');
 // console.log(myNotes);
 
 function getMajor(arrayNotes) {
@@ -57,33 +92,38 @@ function getMajor(arrayNotes) {
 	// return mapNotes;
 }
 
-console.log(getMajor(myNotes));
-
 function majorScaleCreator(scale) {
-	switch (scale) {
-		case 'a':
-			break;
+	// switch (scale) {
+	// 	case 'a':
+	// 		break;
 
-		case 'b':
-			break;
+	// 	case 'b':
+	// 		break;
 
-		case 'c':
-			break;
+	// 	case 'c':
+	// 		break;
 
-		case 'd':
-			break;
+	// 	case 'd':
+	// 		break;
 
-		case 'e':
-			break;
+	// 	case 'e':
+	// 		break;
 
-		case 'f':
-			break;
+	// 	case 'f':
+	// 		break;
 
-		case 'g':
-			break;
+	// 	case 'g':
+	// 		break;
 
-		default:
-			break;
+	// 	default:
+	// 		break;
+	// }
+	for (let i = 0; i < scalesWithSharps.length; i++) {
+		if (scale == scalesWithSharps[i]) {
+		} else if (scale == scalesWithFlats[i]) {
+		} else {
+			// C scale
+		}
 	}
 }
 
